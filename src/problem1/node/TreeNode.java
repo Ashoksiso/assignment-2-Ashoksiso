24b728a4064a49dbf53fdbe95a3dcd3d631dfbea
Ashoksiso <ashok.sisodia_cs18@gla.ac.in>
2020-03-28 11:42:23 -0400

    create tree node class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -2,9 +2,42 @@
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
+ *  Ti/*
+
 package problem1.node;
 
 public class TreeNode {
+    private int key;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+    public TreeNode(int item) {
+        key = item;
+        leftChild = null;
+        rightChild = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
 }
